35c28d61f514a1eb778bd0a08a468ad3502f88d3
Katerina Stredie <stredie_katerina@yahoo.com>
2022-10-07 22:56:34 +0300

    First 3 tasks

---
+++ Maven Pr/io.learnstuff.sample/src/main/java/io/learnstuff/sample/LEARN/App.java
@@ -0,0 +1,13 @@
+package io.learnstuff.sample.LEARN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven Pr/io.learnstuff.sample/src/main/java/io/learnstuff/sample2/string.java
@@ -0,0 +1,13 @@
+package io.learnstuff.sample2;
+
+public class string {
+
+	public static void main(String[] args) {
+		String[] array = {"one", "two", "three"};
+		
+		for (String element: array) {
+			System.out.println(element);
+		}
+	}
+
+}
