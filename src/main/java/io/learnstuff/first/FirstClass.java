2e7627890cc0a9761cf06787c4c344415467fca6
Katerina Stredie <stredie_katerina@yahoo.com>
2022-10-08 09:40:35 +0300

    First Commit

---
+++ src/main/java/io/learnstuff/first/FirstClass.java
@@ -0,0 +1,14 @@
+package io.learnstuff.first;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	int num1 = 10, num2 = 20, sum;
+	
+	sum = num1 + num2;
+	
+	System.out.println(sum);
+
+	}
+
+}
